523a033e780a6976cdc0fc0854da3d4118fce738
JetChan <364950804@qq.com>
2020-02-29 20:45:27 +0800

    delete

--- app-pro/config/proxy.ts
+++ app-pro/config/proxy.ts
@@ -12,10 +12,20 @@ export default {
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
+    '/server-api/': {
+      target: 'http://127.0.0.1:7001',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
   },
   test: {
     '/api/': {
       target: 'https://preview.pro.ant.design',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
+    '/server-api/': {
+      target: 'http://127.0.0.1:7001',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
